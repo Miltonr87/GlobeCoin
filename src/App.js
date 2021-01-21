d71b675ad0ef775f819370fa6ed0c404cf484e9e
Milton Rodrigues <miltonrodrigues713@gmail.com>
2021-01-21 11:23:51 -0300

    Navbar with Styled Components

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Navbar from './components/Navbar';
+import {BrowserRouter as Router} from 'react-router-dom';
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar/index.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { FaBars } from 'react-icons/fa'
+import { Nav, 
+    NavbarContainer, 
+    NavLogo, 
+    MobileIcon, 
+    NavMenu, 
+    NavItem, 
+    NavLinks,
+    NavBtn,
+    NavBtnLink } from './NavbarElements';
+
+
+const Navbar = () => {
+    return (
+        <>
+            <Nav>
+                <NavbarContainer>
+                <NavLogo Link to='/'>
+                    <h1>🌱MintBank</h1>
+                </NavLogo>
+                <MobileIcon>
+                    <FaBars />
+                </MobileIcon>
+                <NavMenu>
+                    <NavItem>
+                        <NavLinks to="about">About</NavLinks>
+                    </NavItem>
+                    <NavItem>
+                        <NavLinks to="discover">Discover</NavLinks>
+                    </NavItem>
+                    <NavItem>
+                        <NavLinks to="services">Services</NavLinks>
+                    </NavItem>
+                    <NavItem>
+                        <NavLinks to="signup">Sign Up</NavLinks>
+                    </NavItem>
+                </NavMenu>
+                <NavBtn>
+                    <NavBtnLink to="/signin">Sign In</NavBtnLink>
+                </NavBtn>
+                </NavbarContainer>
+            </Nav>
+        </>
+    )
+};
+
+export default Navbar;
